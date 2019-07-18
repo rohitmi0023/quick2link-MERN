d0ed92d82de1f029b016383ca3765b515e64298f
rohitmi0023 <rohitracer0023@gmail.com>
2019-07-19 00:11:41 +0530

    added jwt to get the token when registering

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,7 +51,20 @@ router.post(
 			const salt = await bcrypt.genSalt(10);
 			user.password = await bcrypt.hash(password, salt);
 			await user.save();
-			res.send(`User registered`);
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 3500000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(err.mesaage);
 			res.status(500).send(`Server Error...`);
